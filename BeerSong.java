1d4234db8b5f39cdb769f0dc9b09813b752c8125
adamhimself <adam_himself02@hotmail.com>
2016-03-02 10:45:39 +0000

    Allowing for any beer container word to be declared
    Amended the source file so that the end user can declare any beer
    container as string variable in main method, provided the initial word
    declared is plural.

--- BeerSong.java
+++ BeerSong.java
@@ -1,23 +1,23 @@
 public class BeerSong {
 	public static void main (String[] args) {
 		int beerNum = 99;
-		String word = "bottles";
+		String word = "buckets";
 		
 		while (beerNum > 0) {			
 			
 			System.out.println(beerNum + " " + word + " of beer on the wall");
-			System.out.println(beerNum + " " + word + " of beer.");
-			System.out.println("You take one down.");
-			System.out.println("You Pass it round.");
+			System.out.println(beerNum + " " + word + " of beer");
+			System.out.println("You take one down");
+			System.out.println("You Pass it round");
 			beerNum = beerNum - 1;
 			
 			if (beerNum == 1) {
-				word = "bottle"; // singular word				
+				word = word.substring(0, word.length() - 1); // singular word				
 			}
 			if (beerNum > 0) {
 				System.out.println(beerNum + " " + word + " of beer on the wall.");					
 			}	else {
-				System.out.println("No more bottles of beer on the wall.");				
+				System.out.println("No more " + word + "s" + " of beer on the wall.");				
 			} // end else
 			
 		} // end of while loop
