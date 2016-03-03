1229a662090f48594f1897cc60299d854cfbd5fa
adamhimself <adam_himself02@hotmail.com>
2016-03-03 17:36:48 +0000

    Signals Problem
    Updated code

--- SignalsProblem/Signals.java
+++ SignalsProblem/Signals.java
@@ -6,46 +6,46 @@ class Signals {
 		Random Signal = new Random();
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
 		int number;
-		int redTimeRemain;
+		float redTimeRemain;
+		float waitTime = 0;
 		int redCount = 0;
-		int greenCount = 0;
-		int waitTime = 0;
+		int greenCount = 0;		
 		int numberOfSchoolRuns = 0;
 		
 		System.out.println("Enter the number of signals Calvin encounters: ");
 		int numberOfSignals = reader.nextInt(); // end user can declare how many signals are on the way to school.
 		
 						
-		for (int counter=1; counter<=1; counter++) {
-			
+		for (int counter=1; counter<=100000000; counter++) {
+			//System.out.println("School run number: " + counter);
 			int wandUses = 1; // end user can declare how many uses of the magic wand are available.
 			numberOfSchoolRuns++;
 			for (int i=1; i<= numberOfSignals; i++) {
 				number = Signal.nextInt(2);
-				System.out.println("Signal " + i + " encountered!");
+				//System.out.println("Signal " + i + " encountered!");
 				if (number == 0) {
-					System.out.println("The signal is Red!");
+					//System.out.println("The signal is Red!");
 					redCount++;
 					redTimeRemain = (Signal.nextInt(80));
-					System.out.println(redTimeRemain + " seconds remaining until signal change!");
+					//System.out.println(redTimeRemain + " seconds remaining until signal change!");
 					
 					/**The below condition ensures that the wand is used on last red signal regardless of duration. */
-					if (((wandUses > 0) && (redTimeRemain > 20)) || i==numberOfSignals) { 
+					if (((wandUses > 0) && (redTimeRemain >= 32)) || (i==numberOfSignals && wandUses > 0)) { 
 						wandUses = wandUses - 1;
-						System.out.println("Wand used! Signal has been changed to green! no time added!");
+						//System.out.println("Wand used! Signal has been changed to green! no time added!");
 					} else {
 					
 					waitTime = waitTime + (redTimeRemain);
-					System.out.println("Time added = " + waitTime);
+					//System.out.println("Time added = " + redTimeRemain);
 					}
 					
 				} else {
-					System.out.println("The signal is green! No time added!");
+					//System.out.println("The signal is green! No time added!");
 					greenCount++;
 				}
 			}					
 			
-		} // end for loop
+		} // end of for loop
 		System.out.println("The total number of red signals is " + redCount);
 		System.out.println("The total number of green signals is " + greenCount);
 		System.out.println("Total wait time is " + waitTime);
